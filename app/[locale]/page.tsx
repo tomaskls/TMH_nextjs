788b82d5ddcc1e56f3ff1eea952840e12d5d0ef8
tomaskls <tomaskls@hotmail.com>
2024-12-20 14:26:18 +0200

    kuriamas

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,32 +1,13 @@
-import React from 'react';
-import { Snippet } from '@nextui-org/snippet';
-import { Code } from '@nextui-org/code';
-import { title, subtitle } from '@/components/primitives';
+import { title,  } from '@/components/primitives';
 
 export default function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-xl text-center justify-center">
-        <span className={title()}>Make&nbsp;</span>
-        <span className={title({ color: 'violet' })}>beautiful&nbsp;</span>
-        <br />
-        <span className={title()}>
-          websites regardless of your design experience.
-        </span>
-        <div className={subtitle({ class: 'mt-4' })}>
-          Beautiful, fast and modern React UI library.
-        </div>
-      </div>
-
-      <div className="flex gap-3"></div>
-
-      <div className="mt-8">
-        <Snippet hideCopyButton hideSymbol variant="bordered">
-          <span>
-            Get started by editing <Code color="primary">app/page.tsx</Code>
-          </span>
-        </Snippet>
-      </div>
+        <span className={title()}>Puslapis&nbsp;</span>
+        <span className={title({ color: 'violet' })}>kuriamas&nbsp;</span>
+       </div>
+      
     </section>
   );
 }
