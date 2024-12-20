c458b151dcd78f3d7ad1e4296d53522ce48c974e
tomaskls <tomaskls@hotmail.com>
2024-12-20 15:57:02 +0200

    onPresss

--- components/language-switch.tsx
+++ components/language-switch.tsx
@@ -1,30 +1,31 @@
 'use client';
 import React from 'react';
 import { useLocale } from 'next-intl';
-import { usePathname, useRouter } from 'next/navigation';
+import { useRouter, usePathname } from 'next/navigation';
 import { Button } from '@nextui-org/button';
 
 export const LanguageSwitch = () => {
-    const locale = useLocale();
-    const pathname = usePathname();
-    const router = useRouter();
+  const locale = useLocale();
+  const router = useRouter();
+  const pathname = usePathname();
 
-    const toggleLocale = () => {
-        const newLocale = locale === 'en' ? 'lt' : 'en';
-        // Pašaliname esamą lokalę iš kelio ir pridedame naują
-        const newPath = pathname.replace(`/${locale}`, `/${newLocale}`);
+  const toggleLocale = () => {
+    const newLocale = locale === 'en' ? 'lt' : 'en';
+    // Pašaliname esamą lokalę iš kelio
+    const newPath = pathname.replace(`/${locale}`, `/${newLocale}`);
 
-        router.push(newPath);
-    };
+    router.push(newPath);
+  };
 
-    return (
-        <Button 
-            className="text-sm font-normal text-default-600 bg-default-100" 
-            size="sm" 
-            variant="flat"
-            onClick={toggleLocale}
-        >
-            {locale.toUpperCase()}
-        </Button>
-    );
+  return (
+    <Button
+      className=""
+      color='primary'
+      size="sm"
+      variant="solid"
+      onPress={toggleLocale}
+    >
+      {locale.toUpperCase()}
+    </Button>
+  );
 };
