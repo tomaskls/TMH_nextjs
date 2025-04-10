15c36608d0790b9102bec222197b9039946e8ece
tomaskls <tomaskls@hotmail.com>
2025-04-10 17:01:12 +0300

    html lang fix

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -3,16 +3,18 @@ import { Metadata, Viewport } from 'next';
 import { NextIntlClientProvider } from 'next-intl';
 import { notFound } from 'next/navigation';
 import { GoogleAnalytics } from '@next/third-parties/google';
+import clsx from 'clsx';
 
 import { Providers } from './providers';
 
+import { fontSans } from '@/config/fonts';
 import Footer from '@/components/Footer';
 import { Navbar } from '@/components/navbar';
 import CookieConsent from '@/components/CookieConsent';
 
 export const metadata: Metadata = {
   title: "Tomorrow's Media House", 
-  description: "Profesionalus tinklapių kūrimas ir SEO paslaugos, Shopify  el. parduotuvių kūrimas ir priežiūra ",
+  description: "Profesionalus tinklapių kūrimas ir SEO paslaugos, Shopify  el. parduotuvių priežiūra",
   icons: {
     icon: '/favicon.ico',
   },
@@ -48,31 +50,41 @@ export default async function LocaleLayout({
   }
 
   return (
-    <NextIntlClientProvider locale={locale} messages={messages}>
-      <Providers themeProps={{ attribute: 'class', defaultTheme: 'dark' }}>
-        <div className="relative flex flex-col h-screen">
-          <Navbar />
-          <main className="container mx-auto max-w-7xl pt-16 px-4 flex-grow">
-            {children}
-          </main>
-          {/* Configure Google Analytics with default consent mode */}
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('consent', 'default', {
-                  'analytics_storage': 'denied',
-                  'ad_storage': 'denied'
-                });
-              `,
-            }}
-          />
-          <GoogleAnalytics gaId="G-WDJG28XFBK" />
-          <Footer />
-          <CookieConsent />
-        </div>
-      </Providers>
-    </NextIntlClientProvider>
+    <html suppressHydrationWarning lang={locale}>
+      <head />
+      <body
+        className={clsx(
+          'min-h-screen bg-background font-sans antialiased',
+          fontSans.variable
+        )}
+      >
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          <Providers themeProps={{ attribute: 'class', defaultTheme: 'dark' }}>
+            <div className="relative flex flex-col h-screen">
+              <Navbar />
+              <main className="container mx-auto max-w-7xl pt-16 px-4 flex-grow">
+                {children}
+              </main>
+              {/* Configure Google Analytics with default consent mode */}
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('consent', 'default', {
+                      'analytics_storage': 'denied',
+                      'ad_storage': 'denied'
+                    });
+                  `,
+                }}
+              />
+              <GoogleAnalytics gaId="G-WDJG28XFBK" />
+              <Footer />
+              <CookieConsent />
+            </div>
+          </Providers>
+        </NextIntlClientProvider>
+      </body>
+    </html>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import '@/styles/globals.css';
-import clsx from 'clsx';
 // import { GoogleAnalytics } from '@next/third-parties/google'
-
-import { fontSans } from '@/config/fonts';
 
 
 export default function RootLayout({
@@ -11,18 +8,5 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <html suppressHydrationWarning lang="en">
-      <head />
-      <body
-        className={clsx(
-          'min-h-screen bg-background font-sans antialiased',
-          fontSans.variable
-        )}
-      >
-        {children}
-      </body>
-      {/* <GoogleAnalytics gaId=" G-JG3D0F2T25" /> */}
-    </html>
-  );
+  return children; // Tiesiog perduodame vaikus be HTML/body/head tagų
 }
