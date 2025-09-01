dd4bff4b8fda36451a0690084a4f8a57c82f68b3
tomaskls <tomaskls@hotmail.com>
2025-09-01 07:55:42 +0300

    up

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -13,7 +13,7 @@ import { Navbar } from '@/components/navbar';
 import CookieConsent from '@/components/CookieConsent';
 
 export const metadata: Metadata = {
-  title: "Tomorrow's Media House", 
+  title: "Tomorrows Media House", 
   description: "Profesionalus tinklapių kūrimas ir SEO paslaugos, Shopify el. parduotuvių kūrimas, priežiūra ir optimizavimas. Patikimas Shopify partneris verslui.",
   icons: {
     icon: '/favicon.ico',
