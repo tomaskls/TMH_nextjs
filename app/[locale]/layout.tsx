d6d718b131d617739b0c2373a0ae8c96966ac3ee
tomaskls <tomaskls@hotmail.com>
2025-12-13 09:45:05 +0200

    upd params

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -26,7 +26,7 @@ export function generateStaticParams() {
 export async function generateMetadata({
   params,
 }: {
-  params: { locale: string };
+  params: Promise<{ locale: string }>;
 }): Promise<Metadata> {
   const resolvedParams = await params;
   const locale = resolvedParams.locale;
@@ -53,7 +53,7 @@ export default async function LocaleLayout({
   params,
 }: {
   children: React.ReactNode;
-  params: { locale: string };
+  params: Promise<{ locale: string }>;
 }) {
   const resolvedParams = await params;
   const locale = resolvedParams.locale;
